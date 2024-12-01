fe597d15118058517352b757bee0ccd0961f126b
MegaJ <refurb@refurb>
2024-11-30 20:34:50 -0700

    News Section https://codeigniter.com/user_guide/tutorial/news_section.html

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,7 +8,11 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 // Is this really right? To import after other code? Feels wrong
+use App\Controllers\News;
 use App\Controllers\Pages;
+
+$routes->get('news', [News::class, 'index']);
+$routes->get('news/(:segment)', [News::class, 'show']);
 
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
